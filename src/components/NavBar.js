8008b987fccc37aff689117b86ab4fff847e2754
unknown <brunorios314@gmail.com>
2022-07-04 23:44:04 -0300

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+
+
+function NavBar() {
+  return (
+    <>
+    <nav className="nav">
+      <ul>
+        <li>Inicio</li>
+        <li>Catalogo</li>
+        <li>Nuestros locales</li>
+        <li>Mi cuenta</li>
+      </ul>
+    </nav>
+    </>
+  )
+}
+
+export default NavBar
+
+
+
+
+
+
+
